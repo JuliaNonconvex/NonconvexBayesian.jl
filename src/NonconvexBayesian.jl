476cf9af4eeed9b77bc73ec943e47e726513d374
Mohamed Tarek <mohamed82008@gmail.com>
2021-08-31 18:48:27 +1000

    Files generated by PkgTemplates

---
+++ src/NonconvexBayesian.jl
@@ -0,0 +1,5 @@
+module NonconvexBayesian
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using NonconvexBayesian
+using Test
+
+@testset "NonconvexBayesian.jl" begin
+    # Write your tests here.
+end
