eb49dd9d5a16817371d2e03f7b5459c0d37043fd
Mohamed Tarek <mohamed82008@gmail.com>
2021-09-01 23:56:59 +1000

    move code from Nonconvex

--- src/NonconvexBayesian.jl
+++ src/NonconvexBayesian.jl
@@ -1,5 +1,16 @@
 module NonconvexBayesian
 
-# Write your package code here.
+export BayesOptOptions, BayesOptAlg
+
+using Reexport, AbstractGPs, IntervalArithmetic, Parameters
+using Sobol, ChainRulesCore
+@reexport using NonconvexCore
+using NonconvexCore: VecModel, AbstractResult, AbstractOptimizer, getinit
+using NonconvexCore: VectorOfFunctions, IneqConstraint, EqConstraint
+using NonconvexCore: Objective, flatten, @params
+import NonconvexCore: optimize!, Workspace
+using NonconvexIpopt
+
+include("bayesian.jl")
 
 end
